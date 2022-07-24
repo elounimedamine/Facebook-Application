3ee6f6cd0cf858159614555af03fc10d92249e2e
elounimedamine <74010738+elounimedamine@users.noreply.github.com>
2022-07-24 15:26:54 +0100

    Refresh

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:summer_cump_project_2022/pages/auth/login.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
